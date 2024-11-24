0bbe25a59722a3ed1efcc873dca53957b56e2c02
Adam Parr <adamparr.uk@gmail.com>
2024-11-24 20:26:02 +0000

    Add user registration form, view, templates

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name="dashboard"),
+    path('register/', views.register, name='register')
 ]
