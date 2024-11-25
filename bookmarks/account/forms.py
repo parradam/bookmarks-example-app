61ac2808ee2b117f18d78933ada4005aef3c4f5d
Adam Parr <adamparr.uk@gmail.com>
2024-11-25 23:46:05 +0000

    Prevent duplicate emails when signing up and editing profile

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 from django import forms
 from django.contrib.auth import get_user_model
 from .models import Profile
@@ -26,10 +27,27 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
     
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+    
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['first_name', 'last_name', 'email']
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = get_user_model().objects.exclude(
+            id=self.instance.id
+        ).filter(
+            email=data
+        )
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
